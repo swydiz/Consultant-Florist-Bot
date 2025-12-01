75e5fbd70be26e6197044e70298cb9741ae9578d
avariaa <157502226+avariaa@users.noreply.github.com>
2025-12-01 23:13:36 +0300

    Обновлены keyboards.py, env_example.env и settings.py
    keyboards.py: прописаны новые функции get_cart_keyboard и get_help_keyboard
    env_example.env: ссылка на бд изменена под sqllite
    settings.py: изменено чтение и запись admin_ids

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,12 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv("settings.env")
-print("DEBUG TOKEN:", os.getenv("BOT_TOKEN"))
+load_dotenv()
 
 class Settings:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    CHANNEL_URL = os.getenv("CHANNEL_URL")
-    ADMIN_IDS = os.getenv("ADMIN_IDS", "")
+    def __init__(self):
+        self.BOT_TOKEN = os.getenv('BOT_TOKEN')
+        self.DB_FILENAME = os.getenv('DB_FILENAME', 'flowerbot.db')
+        self.CHANNEL_URL = os.getenv('CHANNEL_URL')
+        # Строка с id может быть через запятую, превратить в список интеджеров
+        admin_ids = os.getenv('ADMIN_IDS')
+        if admin_ids:
+            self.ADMIN_IDS = [int(x) for x in admin_ids.split(',') if x.strip().isdigit()]
+        else:
+            self.ADMIN_IDS = []
 
 settings = Settings()

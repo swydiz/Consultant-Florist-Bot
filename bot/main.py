9d516dc0d18cc3e44f471900036ced123fab27ea
avariaa <vare4ka.aa@icloud.com>
2025-12-01 22:23:57 +0300

    Перенесла актуальное с develop в backend

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,24 @@
-from aiogram import Bot, Dispatcher
 import asyncio
 import os
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-
 from bot.handlers.base import BaseHandlers
 from bot.handlers.flowers import FlowerHandlers
+from bot.config.settings import settings
+
 
 load_dotenv()
-
 async def main():
-    bot = Bot(token=os.getenv("BOT_TOKEN"))
+    from bot.database import database
+    await database.init_db()  #инициализация таблиц при старте
+    bot = Bot(token=settings.BOT_TOKEN)
     dp = Dispatcher()
 
+    print(1)
     BaseHandlers(dp)
+    print(2)
     FlowerHandlers(dp)
-    
+
     print("Бот запущен!")
     await dp.start_polling(bot)
 
